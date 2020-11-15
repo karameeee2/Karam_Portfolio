0b4b7e7dbe93887319170933f8b41d91e496e550
karameeee <sezs2119@gmail.com>
2020-11-15 23:45:48 +0900

    2020.11.15 - passport-jwt 로그인 구현(진행중)2

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 8080;
 const cors = require('cors');
 const routes = require('./routes');
 const passport = require('passport');
-const passportConfig = require('./config/passport');
+
 
 // view engine setup
 // app.set('veiws', path.join(__dirname, 'public'));
@@ -16,15 +16,33 @@ app.use(express.static('public'));
 //app.use(express.static)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(session({secret: 'secret code', resave: true, saveUninitialized: false}));
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(session({secret: 'secret code', resave: true, saveUninitialized: false}));
+
 db;
-passportConfig();
 
 // cors허용
 app.use(cors());
 
+
+// passport
+app.use(passport.initialize());
+app.use(passport.session());
+// 처음 세션에 집어넣을때
+passport.serializeUser((user,done) => {
+    console.log('done',user, user.id, user.password)
+    // 브라우저 cookie 에 세션을 넣어주고,
+    // sessions 폴더에(서버에있는) 세션파일이 생성.
+    return done(null, user)
+});
+// 재접속시 (세션유지용)
+passport.deserializeUser((user,done) => {
+    console.log('deserial', user)
+})
+
+const passportConfig = require('./config/passport');
+passportConfig();
+
+// routes
 app.use('/', routes);
 
 app.listen(port, () => {
--- server/config/passport.js
+++ server/config/passport.js
@@ -7,46 +7,29 @@ const UserModel = require('../routes/login/login.controller');
 const db = require('../dbconnection');
 
 module.exports = () => {
-    console.log('asdfasdf')
-    // 등록
     passport.use(new LocalStrategy({
-        usernameField: 'ID',
-        passwordField: 'PASSWORD'
+        usernameField: 'id',
+        passwordField: 'password'
     },
-    function (id, password, done) {
-        console.log('000', id, password)
-        return db.query('SELECT ID, PASSWORD, MIDX FROM SURVEY_MEMBER WHERE ID = "admin1@surveyon.com" AND PASSWORD = "admin1"', (err, rows) => {
-            if(!err) {
-                console.log(11)
-                res.send(rows);
-            } else {
-                console.log(22)
-                console.log(`query error : ${err}`);
-                res.send(err);
-            }
-        })
-        .then(user => {
-            if(!user) {
-                return done(null, false, {message: 'Incorrect id or password'});
-            }
-            return done(null, user, {message: 'Logged In Successfully'});
-        })
-        .catch(err => done(err));
-    }));
+        async function (id, password, done) {
+            console.log(id, password)
+            await db.query(`SELECT ID, PASSWORD FROM SURVEY_MEMBER WHERE ID = "${id}" AND PASSWORD = "${password}"`, (err, rows) => {
+                if (!err) {
+                    console.log('rows', rows);
+                    if (!rows || rows.length <= 0) {
+                        return done(null, false, {
+                            message: '일치하는 사용자가 없습니다.'
+                        })
+                    } else {
 
-    // 토큰발급..?
-    passport.use(new JWTStrategy({
-        jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-        secretOrKey : '1234'
-    },
-    function (jwtPayload, done) {
-        console.log('jwtPayload', jwtPayload);
-        // return UserModel.findOneById(jwtPayload.id)
-        // .then(user => {
-        //     return done(null, user);
-        // })
-        // .catch(err => {
-        //     return done(err);
-        // })
-    }));
+                        return done(null, rows[0])
+                    }
+                } else {
+                    return done(null, err)
+                }
+            })
+            
+
+        }
+    ))
 }
