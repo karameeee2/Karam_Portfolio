b5601949946c11ceeaf4bccf011354060c922117
karameeee <sezs2119@gmail.com>
2020-11-16 17:51:50 +0900

    2020.11.16 - passport-jwt 로그인 기능 구현 ( 진행 중 )

--- server/app.js
+++ server/app.js
@@ -7,16 +7,18 @@ const port = process.env.PORT || 8080;
 const cors = require('cors');
 const routes = require('./routes');
 const passport = require('passport');
-
+require('dotenv').config();
 
 // view engine setup
 // app.set('veiws', path.join(__dirname, 'public'));
 // app.set('view engine', 'ejs');
 app.use(express.static('public'));
-//app.use(express.static)
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-// app.use(session({secret: 'secret code', resave: true, saveUninitialized: false}));
+// app.use(express.static)
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended:true}));
+//app.use(session({secret: 'secret code', resave: true, saveUninitialized: false}));
 
 db;
 
