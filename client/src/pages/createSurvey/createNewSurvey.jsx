ab2b30ef1c1bf1b9b255a9e9976d2f5b811f1341
karameeee <sezs2119@gmail.com>
2021-04-09 18:40:40 +0900

    2021.04.09 - css 사용성 좋게 수정 - 새 설문 등록 페이지 변경된 디자인 반영(진행중)

--- client/src/pages/createSurvey/createNewSurvey.jsx
+++ client/src/pages/createSurvey/createNewSurvey.jsx
@@ -1,28 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Select from 'react-select';
 import '../../css/common/pageTitle.css';
 import '../../css/createSurvey/newSurveyInfoForm.css';
 import '../../css/createSurvey/newSurveyQnaForm.css';
 import '../../css/createSurvey/newSurveyBtnForm.css';
-import appendIcon from '../../assets/icons/appendIcon.svg';
-import calendarIcon from '../../assets/icons/calendarIcon.svg';
+import raidoIcon from '../../assets/icons/radioIcon.svg';
+import checkboxIcon from '../../assets/icons/checkboxIcon.svg';
+import radioSelect from '../../assets/icons/radioSelect.svg';
+import checkboxSelect from '../../assets/icons/checkboxSelect.svg';
+import shortTextSelect from '../../assets/icons/shortTextSelect.svg';
+import longTextSelect from '../../assets/icons/longTextSelect.svg';
 
 const CreateNewSurvey = () => {
-    const inputActive = () => {
+    const inputOnFocus = (e) => {
         console.log('active');
-        let titleInput = document.getElementById('titleInput');
-        let contentInput = document.getElementById('contentInput');
-        // if(this == 'titleInput') {
-        //     titleInput.classList.add('infoActiveFocus');
-        // } else if(this == contentInput) {
-        //     contentInput.classList.add('infoActiveFocus');
-        // }
+        e.target.classList.add('infoActiveFocus');
     }
 
-    const inputNormal = () => {
+    const inputOnBlur = (e) => {
         console.log('normal');
-        let titleInput = document.getElementById('titleInput');
-        titleInput.classList.remove('infoActiveFocus');
+        e.target.classList.remove('infoActiveFocus');
     }
+
+    // select
+    const options = [
+        {value: '객관식 질문'},
+        {value: '체크박스'},
+        {value: '단답형 텍스트'},
+        {value: '장문형 텍스트'}
+    ]
+    const [region, setRegion] = useState(options[0]);
+    const [currentOption, setCurentOption] = useState(null);
+    const onChangeSelect = (item) => {
+        setCurentOption(null);
+        setRegion(item);
+    };
 
     return (
         <>
@@ -45,19 +57,19 @@ const CreateNewSurvey = () => {
                     {/* newSurveyRow = 100% 다 차지, newSurveyHalfRow = 반만 차지 */}
                     <div className="surveyInfoBox">
                         <div className='titleRow'>
-                            <input type='text' id='titleInput' placeholder='제목입력' onFocus={inputActive} onBlur={inputNormal} />
+                            <input type='text' id='titleInput' placeholder='제목입력' onFocus={inputOnFocus} onBlur={inputOnBlur} />
                         </div>
                         <div className='newSurveyRow'>
-                            <textarea rows='2' cols='100' id='contentInput' placeholder='설문조사 참여에 참고할 설문의 목적, 내용 기술' />
+                            <textarea rows='2' cols='100' id='contentInput' placeholder='설문조사 참여에 참고할 설문의 목적, 내용 기술' onFocus={inputOnFocus} onBlur={inputOnBlur} />
                         </div>
                         <div className='newSurveyRow'>
-                            <input type='text' placeholder='태그 (쉼표로 구분해서 입력 > 태그, 태그, 태그)' />
+                            <input type='text' placeholder='태그 (쉼표로 구분해서 입력 > 태그, 태그, 태그)' onFocus={inputOnFocus} onBlur={inputOnBlur} />
                         </div>
                         <div className='newSurveyHalfRow'>
                             <div className="halfRowLeft">
                                 <div className='fileInputBox'>
-                                    <input className='fileName' readOnly type='text' placeholder='썸네일 이미지 파일 첨부' />
-                                    <button><img src={appendIcon} alt="첨부아이콘"/></button>
+                                    <input className='fileName' readOnly type='text' placeholder='썸네일 이미지 파일 첨부' onFocus={inputOnFocus} onBlur={inputOnBlur} />
+                                    <button><p id="appendIcon" className='infoIcon'></p></button>
                                 </div>
                                 <div className="commentBox">
                                     <p className='newSurveyComment'>* 썸네일로 사용할 이미지가 첨부되지 않았을 시에는 임의의 이미지로 대체합니다.</p>
@@ -67,10 +79,10 @@ const CreateNewSurvey = () => {
                             {/* 현재는 text로 입력 */}
                             <div className='halfRowRight'>
                                 <div className="datePickerBox">
-                                    <input className='datePick' readOnly type='text' placeholder='기간' />
+                                    <input className='datePick' readOnly type='text' placeholder='기간' onFocus={inputOnFocus} onBlur={inputOnBlur} />
                                     {/* date picker 사용법 참고 사이트 */}
                                     {/* https://reactnicedates.hernansartorio.com/ */}
-                                    <button><img src={calendarIcon} alt="달력아이콘"/></button>
+                                    <button><p id="calendarIcon" className='infoIcon'></p></button>
                                 </div>
                                 <div className="commentBox">
                                     <p className='newSurveyComment'>* 기간은 최소1일부터 최대 30일까지 가능합니다.</p>
@@ -94,12 +106,20 @@ const CreateNewSurvey = () => {
                                 <input type="text" placeholder='질문' />
                             </div>
                             <div className='typeBox'>
-                                <select defaultValue={1}>
-                                    {/* value 값은 임의로 숫자로해놨어욘 */}
-                                    <option value={1}>주관식</option>
-                                    <option value={2}>객관식(1개 선택)</option>
-                                    <option value={3}>객관식(중복 선택 가능)</option>
-                                </select>
+                                <Select
+                                    value={region}
+                                    onChange={onChangeSelect}
+                                    options={options}
+                                    getOptionValue={(option) => option.value}
+                                    getOptionLabel={(option) => option.value}
+                                />
+                                {/* <select defaultValue={1}>
+                                    value 값은 임의로 숫자로해놨어욘
+                                    <option value={1}>객관식 질문</option>
+                                    <option value={2}>체크박스</option>
+                                    <option value={3}>단답형 텍스트</option>
+                                    <option value={4}>장문형 텍스트</option>
+                                </select> */}
                             </div>
                         </div>
                         <div className='answerDeleteWrap'>
