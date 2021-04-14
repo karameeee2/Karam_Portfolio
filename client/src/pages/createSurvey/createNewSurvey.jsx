f797dd6e8d71745b21d1a3f7d9a545e9a70ed01b
karameeee <sezs2119@gmail.com>
2021-04-14 18:29:10 +0900

    2021.04.14 - 드롭다운 리스트 구현 (완료) - 질문 컴포넌트 추가 기능 구현 (진행 중)

--- client/src/pages/createSurvey/createNewSurvey.jsx
+++ client/src/pages/createSurvey/createNewSurvey.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Select from 'react-select';
 import '../../css/common/pageTitle.css';
 import '../../css/createSurvey/newSurveyInfoForm.css';
@@ -8,6 +8,7 @@ import radioSelect from '../../assets/icons/radioSelect.svg';
 import checkboxSelect from '../../assets/icons/checkboxSelect.svg';
 import shortTextSelect from '../../assets/icons/shortTextSelect.svg';
 import longTextSelect from '../../assets/icons/longTextSelect.svg';
+import arrowDown from '../../assets/icons/arrowDown.svg'
 
 const CreateNewSurvey = () => {
     const inputOnFocus = (e) => {
@@ -21,17 +22,122 @@ const CreateNewSurvey = () => {
     }
 
     // select
+    const styles = {
+        // 옵션들에 대한 스타일 정의
+        option: (styles, {isFocused, isDisabled}) => {
+            return {
+                backgroundColor: isFocused ? '#F0F0F0' : '',
+                color: isDisabled ? '#C7C7C7' : '',
+                cursor: isDisabled ? 'default' : 'cursor',
+                padding: '0 24px'
+            };
+        },
+        // styles =>  ...styles는 이 라이브러리가 쓰고있는 기본 스타일을 일단 복사한다는 의미
+        // control: 인풋창(select 태그라고 생각하면 됨)
+        control: (styles) => (
+            {
+                ...styles, height: "48px", cursor: "pointer", padding: '0 24px', boxShadow: 'none', 
+                border: '1px solid #C7C7C7', borderRadius: '4px'
+            }
+        ),
+
+        // container: option, control을 감싸고 있는 부모요소 (React-select 컴포넌트 최상위부모컨테이너)
+        // 여기서 width 값을 결정하면 됨
+        container: (styles) => (
+            {
+                ...styles, width: "276px", height: "48px"
+            }
+        ),
+
+        valueContainer: (styles) => (
+            {
+                ...styles, height: '48px', padding: '0'
+            }
+        ),
+        indicatorSeparator: (styles) => (
+            {
+                ...styles, display: 'none', 
+            }
+        ),
+        menuList: (styles) => (
+            {
+                ...styles, lineHeight: '48px', padding: '0'
+            }
+        ),
+        
+    }
+
+    // 선택 목록 (option 태그)
+    // value는 요소 선택했을 때 넘어오는 값
+    // label 은 보여줄 컴포넌트
     const options = [
-        {value: '객관식 질문'},
-        {value: '체크박스'},
-        {value: '단답형 텍스트'},
-        {value: '장문형 텍스트'}
+        {
+            value: '1',
+            label: (
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <img src={radioSelect} style={{ width: '24px', marginRight: '12px' }} alt='객관식질문아이콘' />{''}객관식 질문
+                </div>
+            )
+        },
+        {
+            value: '2',
+            label: (
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <img src={checkboxSelect} style={{ width: '24px', marginRight: '12px' }} alt='체크박스아이콘' />{''}체크박스
+                </div>
+            ),
+            // isDisabled: true
+        },
+        {
+            value: '3',
+            label: (
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <img src={shortTextSelect} style={{ width: '24px', marginRight: '12px' }} alt='단답형텍스트아이콘' />{''}단답형 텍스트
+                </div>
+            ),
+            // isDisabled: true
+        },
+        {
+            value: '4',
+            label: (
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <img src={longTextSelect} style={{ width: '24px', marginRight: '12px' }} alt='장문형텍스트아이콘' />{''}장문형 텍스트
+                </div>
+            ),
+            // isDisabled: true
+        },
     ]
-    const [region, setRegion] = useState(options[0]);
-    const [currentOption, setCurentOption] = useState(null);
+    
     const onChangeSelect = (item) => {
-        setCurentOption(null);
-        setRegion(item);
+        let radioSelected = document.getElementsByClassName('selectOne')[0];
+        let checkSelected = document.getElementsByClassName('selectMulti')[0];
+        let shortSelected = document.getElementsByClassName('shortText')[0];
+        let longSelected = document.getElementsByClassName('longText')[0];
+        if(item.value === '1') {
+            console.log('객관식 질문');
+            radioSelected.style.display = 'block';
+            checkSelected.style.display = 'none';
+            shortSelected.style.display = 'none';
+            longSelected.style.display = 'none';
+        } else if(item.value === '2') {
+            console.log('체크박스');
+            radioSelected.style.display = 'none'; 
+            checkSelected.style.display = 'block';
+            shortSelected.style.display = 'none';
+            longSelected.style.display = 'none';
+        } else if(item.value === '3') {
+            console.log('단답형 텍스트');
+            radioSelected.style.display = 'none'; 
+            checkSelected.style.display = 'none';
+            shortSelected.style.display = 'block';
+            longSelected.style.display = 'none';
+        } else if(item.value === '4') {
+            console.log('장문형 텍스트');
+            radioSelected.style.display = 'none'; 
+            checkSelected.style.display = 'none';
+            shortSelected.style.display = 'none';
+            longSelected.style.display = 'block';
+        }
     };
 
     return (
@@ -105,11 +211,11 @@ const CreateNewSurvey = () => {
                             </div>
                             <div className='typeBox'>
                                 <Select
-                                    value={region}
-                                    onChange={onChangeSelect}
+                                    styles={styles}
                                     options={options}
-                                    getOptionValue={(option) => option.value}
-                                    getOptionLabel={(option) => option.value}
+                                    onChange={onChangeSelect}
+                                    isSearchable={false}
+                                    defaultValue={options[0]}
                                 />
                             </div>
                         </div>
@@ -146,29 +252,7 @@ const CreateNewSurvey = () => {
                                 <a href='#!' className='addTextField'>기타 추가</a>
                             </span>
                         </div>
-                        <div className='deleteBox'>
-                            <button className='deleteBtn'>
-                                <p className="deleteIcon"></p>
-                            </button>
-                        </div>
-                    </div>
-                    {/* --------------------------------------------------------------------------------- */}
-                    <div className='surveyForm'>
-                        {/* surveyForm = 테두리 스타일링, padding 값 */}
-                        <div className='questionTypeWrap'>
-                            <div className='questionBox'>
-                                <input type="text" placeholder='질문' onFocus={inputOnFocus} onBlur={inputOnBlur} />
-                            </div>
-                            <div className='typeBox'>
-                                <Select
-                                    value={region}
-                                    onChange={onChangeSelect}
-                                    options={options}
-                                    getOptionValue={(option) => option.value}
-                                    getOptionLabel={(option) => option.value}
-                                />
-                            </div>
-                        </div>
+
                         {/* 체크박스 */}
                         <div className='answerBox selectMulti'>
                             <span className="optionBox">
@@ -202,61 +286,16 @@ const CreateNewSurvey = () => {
                                 <a href='#!' className='addTextField'>기타 추가</a>
                             </span>
                         </div>
-                        <div className='deleteBox'>
-                            <button className='deleteBtn'>
-                                <p className="deleteIcon"></p>
-                            </button>
-                        </div>
-                    </div>
-                    {/* --------------------------------------------------------------------------------- */}
-                    <div className='surveyForm'>
-                        {/* surveyForm = 테두리 스타일링, padding 값 */}
-                        <div className='questionTypeWrap'>
-                            <div className='questionBox'>
-                                <input type="text" placeholder='질문 입력'onFocus={inputOnFocus} onBlur={inputOnBlur} />
-                            </div>
-                            <div className='typeBox'>
-                                <Select
-                                    value={region}
-                                    onChange={onChangeSelect}
-                                    options={options}
-                                    getOptionValue={(option) => option.value}
-                                    getOptionLabel={(option) => option.value}
-                                />
-                            </div>
-                        </div>
                         {/* 단답형 텍스트 */}
                         <div className='answerBox shortText'>
                             <input type="text" placeholder='단답형 텍스트' onFocus={inputOnFocus} onBlur={inputOnBlur} />
-                        </div>
-                        <div className='deleteBox'>
-                            <button className='deleteBtn'>
-                                <p className="deleteIcon"></p>
-                            </button>
-                        </div>
-                        
-                    </div>
-                    {/* --------------------------------------------------------------------------------- */}
-                    <div className='surveyForm'>
-                        {/* surveyForm = 테두리 스타일링, padding 값 */}
-                        <div className='questionTypeWrap'>
-                            <div className='questionBox'>
-                                <input type="text" placeholder='질문' onFocus={inputOnFocus} onBlur={inputOnBlur} />
-                            </div>
-                            <div className='typeBox'>
-                                <Select
-                                    value={region}
-                                    onChange={onChangeSelect}
-                                    options={options}
-                                    getOptionValue={(option) => option.value}
-                                    getOptionLabel={(option) => option.value}
-                                />
-                            </div>
                         </div>
                         {/* 장문형 텍스트 */}
                         <div className='answerBox longText'>
                             <input type="text" placeholder='장문형 텍스트' onFocus={inputOnFocus} onBlur={inputOnBlur} />
                         </div>
+
+
                         <div className='deleteBox'>
                             <button className='deleteBtn'>
                                 <p className="deleteIcon"></p>
