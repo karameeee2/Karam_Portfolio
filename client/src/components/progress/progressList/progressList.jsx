c7700a68a59fd525ba19f2f3a1a8805a25e70c25
karameeee <sezs2119@gmail.com>
2021-05-25 19:12:24 +0900

    2021.05.25 - pagination 구현 (진행 중)

--- client/src/components/progress/progressList/progressList.jsx
+++ client/src/components/progress/progressList/progressList.jsx
@@ -4,22 +4,26 @@ import Progress from '../../../pages/progress/progressList/progressList';
 
 const ProgressListComponent = () => {
     // useEffect의 두번째 인수를 빈배열로 두면 컴포넌트가 렌더링될때만 useEffect 안의 구문이 실행된다.
+    const [surveyList, setSurveyList] = useState([]);
+    // const [offset, setOffset] = useState(0);
+
     useEffect(() => {
         getProgressList();
     }, [])
     
-    const [surveyList, setSurveyList] = useState([]);
     const getProgressList = () => {
         Axios.get('http://localhost:8080/api/selectSurveyList')
         .then((res) => {
             setSurveyList(res.data);
+            console.log('surveyTotal', res.data.length);
         })
         .catch((err)=>{
             console.log('에러낫음', err.res, err)
         })
     }
+
     return (
-        <Progress pageTitle={ '진행중인 설문' } surveyList={surveyList} />
+        <Progress pageTitle={ '진행중인 설문' } surveyList={surveyList}  />
     )
 }
 
--- client/src/pages/progress/progressList/progressList.jsx
+++ client/src/pages/progress/progressList/progressList.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import { CommonUtils } from '../../../utils/common';
 import ReactPaginate from 'react-paginate';
 import '../../../css/common/pageTitle.css';
 import '../../../css/common/previewList.css';
 import '../../../css/progress/progressList/progressPreview.css';
-import '../../../css/progress/progressList/progressPreview.css';
+import '../../../css/common/pagination.css';
 import thumbnail1 from '../../../assets/thumbnail/thumbnail_basic.png';
 import termIcon from '../../../assets/icons/termIcon.svg';
 import hashIcon from '../../../assets/icons/hashIcon.svg';
@@ -14,7 +14,43 @@ import arrowNext from '../../../assets/icons/arrowNext.svg';
 import arrowPrev from '../../../assets/icons/arrowPrev.svg';
 
 const ProgressList = (props) => {
-    const progressPreview = props.surveyList;
+    const progressPreview = props.surveyList; // 전달받은 전체배열
+    const progressCount = props.surveyList.length;
+    console.log('progressCount>>>>>>>>>', progressCount);
+    const item_per_page = 6; // 한페이지에 보여줘야할 개수
+    
+    const [activeProgressList, setActiveProgressList] = useState([]); // 보여져야할 배열
+
+    // 처음엔 페이지네이션 처음 숫자를 클릭한것과 같은 효과를 주기 위함임.
+    useEffect(() => {
+        console.log('몇번실행?');
+        handlePageClick({selected: 0})
+    }, [progressPreview])
+
+    const handlePageClick = (data) => {
+        let selected = data.selected; // 페이지네이션 숫자 클릭 시 넘어오는 데이터 {selected: ~}
+        let offset = Math.ceil(selected * item_per_page); // 건너뛸개수
+
+        // this.setState({ offset: offset }, () => {
+        //     this.loadCommentsFromServer();
+        //     //currentPage (현재페이지), offset (건너뛸 개수), item_per_page (페이지당 보여져야할 개수),total
+        //     // 1, 0, 10, 
+        // });
+
+        // progressPreview - data list
+        let arr = [...progressPreview]; // 1. 전체배열을 복사
+        
+        // 2. selected 값과 offset 을 활용해서 몇개씩 보여져야할 지 배열을 편집
+        if(!selected) {
+            arr = arr.splice(0, item_per_page)
+        } else {
+            arr = arr.splice(offset, selected * item_per_page); // 문제의 소지가 있을 수도..
+        }
+
+        // 3. 보여져야할 배열 state update
+        setActiveProgressList(arr)
+    };
+
     return (
         <>
             {/* pageTitle */}
@@ -32,7 +68,7 @@ const ProgressList = (props) => {
                 <div className="progressPrevWrap">
                     <div className="previewContainer">
                         <ul className="previewWrap">
-                            {progressPreview.map((item) => {
+                            {activeProgressList.map((item) => {
                                 return (
                                     <li className="prevBox" key={item.SIDX}>
                                         <img className="prevThumbnail" src={item.SIMG || thumbnail1} alt='설문 썸네일 이미지' onError={e => e.target.src = thumbnail1} />
@@ -55,18 +91,18 @@ const ProgressList = (props) => {
             </section>
 
             {/* pagination */}
-            {/* <ReactPaginate 
-                // pageCount={Math.ceil(totalRecords / 6)}
-                pageRangeDisplayed={6}
-                breakLabel={''}
-                previousLabel={arrowPrev+'이전'}
-                nextLabel={arrowNext+'다음'}
-                // onPageChange={changePage}
+            <ReactPaginate 
+                previousLabel={'이전'}
+                nextLabel={'다음'}
+                breakLabel={'...'}
+                pageCount={Math.ceil(progressCount / item_per_page)}
+                pageRangeDisplayed={item_per_page}
                 containerClassName={'paginationWrap'}
                 activeClassName={'currentPage'}
                 previousClassName={'prevPage'}
                 nextClassName={'nextPage'}
-            /> */}
+                onPageChange={handlePageClick}
+            />
             
         </>
         // <div>
