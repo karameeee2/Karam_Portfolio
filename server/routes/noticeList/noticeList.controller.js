29daf8084c6e8fd1f42c8401cd519a91d3fc59f4
karameeee <sezs2119@gmail.com>
2021-06-03 19:01:58 +0900

    2021.06.03 - db 연결 테스트

--- server/routes/noticeList/noticeList.controller.js
+++ server/routes/noticeList/noticeList.controller.js
@@ -1,12 +1,17 @@
 const db = require('../../dbconnection');
 
 exports.noticeList = (req, res, next) => {
+    if(!db) {
+        res.status(500).send('db connection failed');
+        return;
+    }
+
     db.query('SELECT NIDX, NSUBJECT, CREATEDATE, HIT FROM SURVEY_NOTICE ORDER BY NIDX DESC', (err, rows) => {
         if(!err) {
             res.send(rows);
         } else {
             console.log(`query error : ${err}`);
-            res.send(err);
+            res.status(500).send(err);
         }
     })
 }
